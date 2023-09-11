27c95fc8bc6b022b26e4f818bf71522c46dc9d15
Benny Mendoza <bennybc9387@gmail.com>
2023-09-11 17:49:40 -0600

    added try/catch block

--- hello-world/src/app/fetch.component.ts
+++ hello-world/src/app/fetch.component.ts
@@ -3,21 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './fetch.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-
 export class Fetch {
-    fetch = 'hello-world, this is Benny from Fetch';
-  }
+  fetch = 'hello-world, this is Benny from Fetch';
+}
 
-async function getWeather () {
+async function getWeather() {
+  try {
     const data = await fetch('https://reqres.in/api/users/2');
     const response = await data.json();
     console.log(response.data.email);
-    setTimeout( ()=>
-        console.log('this is benny again after 2 seconds'), 2000)
-    console.log('testing dumb api example console')
-   
+    setTimeout(() => console.log('this is benny again after 2 seconds'), 2000);
+    console.log('testing dumb api example console');
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-getWeather()
+getWeather();
