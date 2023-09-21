d1fce9c7b5ada797b45ec83bda31b350f93f182a
Benny Mendoza <bennybc9387@gmail.com>
2023-09-21 00:11:57 -0600

    adding weather component

---
+++ hello-world/src/app/weather.component.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './weather.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class Weather {
+  weather = `hello, this is  ${getWeather()}   `;
+}
+
+function getWeather() {
+  return 'this is my testing';
+}
+
+const nums = [1, 2, 4, 5, 6, 7, 8, 9];
+
+function multiply(value: any) {
+  return value + 2;
+}
+console.log(nums.map(multiply) + 'benny');
+
+console.log(nums.map);
+
+function callback(arr: any) {
+  return arr + ' benny is here counting ';
+}
+
+const newNums = nums.map(callback);
+console.log(newNums);
+
+const numbers = [1, 4, 9];
+const doubles = numbers.map((num) => num * 2);
+
+console.log(doubles);
+
+const kvArray = [
+  { first: 'benny', last: 'mendoza' },
+  { first: 'sarah', last: 'maddock' },
+  { first: 'billie', last: 'bobby' },
+];
+
+console.log(kvArray);
+const reformattedArray = kvArray.map(({ first, last }) => ({ [first]: last }));
+
+console.log(reformattedArray); // [{ 1: 10 }, { 2: 20 }, { 3: 30 }]
+console.log(kvArray);
